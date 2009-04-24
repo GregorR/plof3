d66326fd62e96f610655675b13e83fe123282539
Elliott Hird <penguinofthegods@gmail.com>
2009-04-24 01:14:55 +0100

    Break everything (for the hell of it, obviously.)

--- cplof/src/plof.h
+++ cplof/src/plof.h
@@ -30,16 +30,22 @@
 
 #include <gc/gc.h>
 
+enum {
+    PLOF_OBJECT,
+    PLOF_RAW_DATA,
+    PLOF_ARRAY_DATA
+} PlofType;
+
+typedef struct Plof Plof;
+
+struct Plof;
 struct PlofObject;
-struct PlofReturn;
-struct PlofOHashTable;
-struct PlofData;
+struct PlofHashTable;
+struct PlofRawData;
+struct PlofArrayData;
 
 /* search path for include, should be a null-terminated array of strings */
 extern unsigned char **plofIncludePaths;
-
-#define PLOF_DATA_RAW 0
-#define PLOF_DATA_ARRAY 1
 
 #define PSL_FILE_MAGIC "\x9E\x50\x53\x4C\x17\xF2\x58\x8C"
 
@@ -101,77 +107,48 @@ extern unsigned char **plofIncludePaths;
 
 /* All functions accessible directly from Plof should be of this form
  * args: context, arg */
-typedef struct PlofReturn (*PlofFunction)(struct PlofObject *, struct PlofObject *);
+typedef struct PlofReturn (*PlofFunction)(Plof *, Plof *);
 
-/* A Plof object
- * data: raw or array data associated with the object
- * hashTable: hash table of name->value associations
- * itable: immediate table, for objects with known members */
-struct PlofObject {
-    struct PlofObject *parent;
-    struct PlofData *data;
-    struct PlofOHashTable *hashTable;
-    struct PlofObject *itable[1];
-};
+/* Plof is just zis structure, you know? */
+struct Plof {
+    enum PlofType type; 
+    union {
+        struct PlofObject {
+            Plof *parent;
+            struct PlofHashTable {
+                size_t hashedName;
+                size_t nameLength;
+                Plof *value;
+                struct PlofHashTable *left, *right;
+            } hashTable;
+        } object;
 
-/* The return type from Plof functions, which specifies whether a value is
- * being returned or thrown
- * ret: The actual return value
- * isThrown: 1 if this is a throw */
+        struct PlofRawData {
+            size_t length;
+            size_t hash;
+            unsigned char *data;
+        } rawData;
+
+        struct PlofArrayData {
+            size_t length;
+            Plof **elems;
+        };
+    } data;
+}
+
 struct PlofReturn {
-    struct PlofObject *ret;
+    struct PlofObject *value;
     unsigned char isThrown;
 };
 
-/* Hash tables, associating an int hashed name with a char * real name and
- * value */
-struct PlofOHashTable {
-    size_t hashedName;
-    size_t namelen;
-    unsigned char *name;
-    struct PlofObject *value;
-    struct PlofOHashTable *left, *right;
-};
-
-/* "Superclass" for Plof data
- * type: Specifies which type this is */
-struct PlofData {
-    int type;
-};
-
-/* Standard Plof raw data
- * type: Should always be PLOF_DATA_RAW
- * length: The length of the data
- * data: The data itself (of course)
- * idata: Any data stored by the interpreter (e.g. a compiled version)
- * proc: The fully-compiled function of this data */
-struct PlofRawData {
-    int type;
-    size_t length;
-    unsigned char *data;
-    size_t hash;
-    void *idata;
-    PlofFunction proc;
-};
-
-/* Array data
- * type: Should always be PLOF_DATA_ARRAY
- * length: The length of the array
- * data: The array */
-struct PlofArrayData {
-    int type;
-    size_t length;
-    struct PlofObject **data;
-};
-
 /* Major Plof constants */
-extern struct PlofObject *plofNull, *plofGlobal;
+extern Plof *plofNull, *plofGlobal;
 
 /* The standard PSL interpreter */
 struct PlofReturn interpretPSL(
-        struct PlofObject *context,
-        struct PlofObject *arg,
-        struct PlofObject *pslraw,
+        Plof *context,
+        Plof *arg,
+        Plof *pslraw,
         size_t pslaltlen,
         unsigned char *pslalt,
         int generateContext,
@@ -181,9 +158,9 @@ struct PlofReturn interpretPSL(
 size_t plofHash(size_t slen, unsigned char *str);
 
 /* Copy the content of one object into another (for 'combine') */
-void plofObjCopy(struct PlofObject *to, struct PlofOHashTable *from);
+void plofObjCopy(Plof *to, Plof *from);
 
 /* Make an array of the list of members of an object (for 'members') */
-struct PlofArrayData *plofMembers(struct PlofObject *of);
+Plof *plofMembers(Plof *of);
 
 #endif
