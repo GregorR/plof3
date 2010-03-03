fea5ef3d9998e4ac5bd669725c53bf14ab23c3f1
Gregor Richards <Richards@codu.org>
2010-03-02 20:46:07 -0500

    Updated copyright headers that should specify 2010

--- cplof/src/gen.c
+++ cplof/src/gen.c
@@ -1,7 +1,7 @@
 /*
  * Wrapper for generating particular files that need every PSL instruction
  *
- * Copyright (C) 2009 Gregor Richards
+ * Copyright (C) 2009, 2010 Gregor Richards
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- cplof/src/plof/plof.h
+++ cplof/src/plof/plof.h
@@ -2,7 +2,7 @@
  * This header has generally accessible structs and functions, e.g. the
  * functions in psl.c
  *
- * Copyright (C) 2007, 2008, 2009 Gregor Richards
+ * Copyright (C) 2007, 2008, 2009, 2010 Gregor Richards
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
