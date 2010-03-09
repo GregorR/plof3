5455e764c64ca922f62d337c30bb7b18b57026c3
Gregor Richards <Richards@codu.org>
2010-03-09 13:48:20 -0500

    Pedantic pedantic pedantic.

--- cplof/src/packrat.c
+++ cplof/src/packrat.c
@@ -317,7 +317,7 @@ struct ParseResult **packratNonterminal(struct ParseContext *ctx,
 
     if (packratWarnAmbiguous && result.bufused > 2) {
         fprintf(stderr, "Ambiguity: Production %s returned %d at %s:%d:%d\n",
-                (char *) production->name, result.bufused - 1,
+                (char *) production->name, (int) result.bufused - 1,
                 (char *) file, (int) line + 1, (int) col + 1);
     }
 
