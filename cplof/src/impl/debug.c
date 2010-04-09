d554c0a6b8d8dd33f58e958e224db78a9c0ec6db
Gregor Richards <Richards@codu.org>
2010-04-09 14:40:29 -0400

    cplof/src/impl/debug.c: psl { debug } is now dump-stack-trace, 'cuz I wanted it to be.

--- cplof/src/impl/debug.c
+++ cplof/src/impl/debug.c
@@ -2,7 +2,22 @@ label(interp_psl_debug);
     DEBUG_CMD("debug");
     {
         static unsigned int debugCount = 0;
-        fprintf(stderr, "%u\r", ++debugCount);
+        /*fprintf(stderr, "%u\r", ++debugCount);*/
+        pid_t pid;
+        char *corefn, *pidstr;
+
+        debugCount++;
+        corefn = GC_MALLOC_ATOMIC(32);
+        snprintf(corefn, 16, "core.%u", debugCount);
+        pidstr = GC_MALLOC_ATOMIC(32);
+        snprintf(pidstr, 16, "%u", getpid());
+
+        pid = fork();
+        if (pid) {
+            waitpid(pid, NULL, 0);
+        } else {
+            execlp("gcore", "gcore", "-o", corefn, pidstr, NULL);
+        }
     }
     STEP;
 
