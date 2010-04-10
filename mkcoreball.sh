898d2f6611409756c1e9e7dcc4a640cbcc250267
Gregor Richards <Richards@codu.org>
2010-04-10 15:50:48 -0400

    mkcoreball.sh: Included a backtrace in the coreball.

--- mkcoreball.sh
+++ mkcoreball.sh
@@ -4,7 +4,13 @@ do
     if [ ! -e "coreball.$i.tar.bz2" ]
     then
         hg id > hgid
-        tar jcf coreball.$i.tar.bz2 hgid core cplof/src/cplof `hg status -nam`
+
+        echo 'Creating backtrace...'
+        gdb --batch -x autotests/bt.gdb ./cplof/src/cplof core > backtrace
+
+        echo 'Creating coreball...'
+        tar jcf coreball.$i.tar.bz2 hgid core backtrace cplof/src/cplof `hg status -nam`
+
         echo coreball.$i.tar.bz2
         exit 0
     fi
