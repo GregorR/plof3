fc4b27029ff4e266f71755fc943f66cfce337731
Gregor Richards <Richards@codu.org>
2010-05-15 11:51:09 -0400

    Array arities are now "baked in"

--- cplof/src/optim/array.c
+++ cplof/src/optim/array.c
@@ -1,5 +1,34 @@
-/* too weird to do anything with */
 ARITY(0)
 PUSHES(1)
 LEAKP
 LEAKALL
+
+#ifdef PSL_OPTIM
+/* get the arity manually */
+if (cpsli >= 2 && cpsl[cpsli-2] == compileLabels[label_psl_integer] && cpsl[cpsli-1] != NULL) {
+    /* good, this is an integer literal, as it must be */
+    cpsl[cpsli-2] = cpsl[cpsli];
+    cpsli -= 2;
+
+    /* now we can get the arity */
+#if defined(PLOF_BOX_NUMBERS)
+    rd = (struct PlofRawData *) cpslargs[(int) (size_t) cpsl[cpsli+1]];
+    arity = *((ptrdiff_t *) rd->data);
+#elif defined(PLOF_FREE_INTS)
+    arity = ASINT(cpslargs[(int) (size_t) cpsl[cpsli+1]]);
+#endif
+
+} else {
+    rd = newPlofRawData(128);
+    sprintf((char *) rd->data, "'array' instruction with nonstatic size.");
+    a = newPlofObject();
+    a->parent = plofNull;
+    a->data = (struct PlofData *) rd;
+
+    ret.ret = newPlofObject();
+    ret.ret->parent = plofNull;
+    plofWrite(ret.ret, (unsigned char *) PSL_EXCEPTION_STACK, plofHash(sizeof(PSL_EXCEPTION_STACK)-1, (unsigned char *) PSL_EXCEPTION_STACK), a);
+    ret.isThrown = 1;
+    return ret;
+}
+#endif
